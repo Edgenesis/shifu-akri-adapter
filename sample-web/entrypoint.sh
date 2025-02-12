f76a06e3c9c1e8c3c6a2eb4ba0547dc1a6c79f3c
Tom Qin <xqin@edgenesis.com>
2025-02-12 18:35:08 +0800

    update web to take env as base url (#3)

---
+++ sample-web/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Substitute variables in index.html
+envsubst '${BASE_URL}' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
+# Start nginx
+nginx -g 'daemon off;'
